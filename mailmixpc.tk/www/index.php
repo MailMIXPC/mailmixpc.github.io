65292acf3aae945deb04f01cea88e3d61ee501bb
MailMIXPC <46049257+MailMIXPC@users.noreply.github.com>
2018-12-21 22:40:55 +0500

    Add files via upload

---
+++ mailmixpc.tk/www/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="author" content="The Sabotage Rebellion hackers" />
+    <link href="reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+	<title>MailMIXPC - Интернет-Магазин И Сборка Компьютеров</title>
+</head>
+
+<body>
+
+<div id="block-body"></div>
+
+
+</body>
+</html>
